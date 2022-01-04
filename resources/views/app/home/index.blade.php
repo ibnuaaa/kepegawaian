d088b88755636a432470f2bd291c6a1026145ffc
Chaisir Ibnu Supraja <pkusumanegara@gmail.com>
2022-01-04 10:01:25 +0700

    Dashboard

--- resources/views/app/home/index.blade.php
+++ resources/views/app/home/index.blade.php
@@ -6,7 +6,8 @@
 @section('content')
     <div class="container-fluid container-fixed-lg">
         <div class="card card-white">
-            <div class="card-body text-center" style="font-size: 40px;">
+            <div class="card-body text-center" style="font-size: 20px;">
+                <br /><br /><br />
                 <b>
                     <div class="m-b-5">
                         SELAMAT DATANG
@@ -15,7 +16,7 @@
                         di
                     </div>
                     <div class="m-b-5">
-                        <span style="font-weight: bold; font-size: 40px;">SIMPEG <i></i></span>
+                        <span style="font-weight: bold; font-size: 40px;">SIMPEG - RSPON<i></i></span>
                     </div>
                     <div class="m-b-5">
                         <span style="font-weight: bold; font-size: 40px;">Sistem Informasi Manajemen Kepegawaian</span> <br>
@@ -24,6 +25,7 @@
                         <img style="width: 200px;" src="/assets/img/logo-rspon.png">
                     </div>
                     RS PUSAT OTAK NASIONAL
+                    <br /><br /><br /><br />
                 </b>
             </div>
         </div>
