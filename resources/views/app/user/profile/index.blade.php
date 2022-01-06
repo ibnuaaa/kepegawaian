80165d39bfba111730059af0ff4fe1043779413d
Chaisir Ibnu Supraja <pkusumanegara@gmail.com>
2022-01-06 11:37:02 +0700

    Membuat Halaman Profil User

--- resources/views/app/user/profile/index.blade.php
+++ resources/views/app/user/profile/index.blade.php
@@ -8,11 +8,11 @@
 
     <!-- PAGE-HEADER -->
     <div class="page-header">
-        <h1 class="page-title">Profil</h1>
+        <h1 class="page-title">Profil User</h1>
         <div>
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Profil</li>
+                <li class="breadcrumb-item active" aria-current="page">Profil User</li>
             </ol>
         </div>
     </div>
@@ -20,50 +20,46 @@
 
     <!-- ROW-1 -->
     <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xl-12">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xl-12">
-                    <div class="card overflow-hidden">
-                      <div class="card-body">
-                          <div class="row">
-                              <div class="col-3">
-                                  <div class="form-group">
-                                      <label>Nama</label>
-                                  </div>
-                              </div>
-                              <div class="col-9">
-                                  <div class="form-group form-group-default">
-                                      {{ $data->name }}
-                                  </div>
-                              </div>
-                          </div>
-                          <div class="row">
-                              <div class="col-3">
-                                  <div class="form-group">
-                                      <label>Jabatan</label>
-                                  </div>
-                              </div>
-                              <div class="col-9">
-                                  <div class="form-group form-group-default">
-                                      <?php echo $data->position['name']; ?>
-                                  </div>
-                              </div>
-                          </div>
-                          <div class="row">
-                              <div class="col-3">
-                                  <div class="form-group">
-                                      <label>Username</label>
-                                  </div>
-                              </div>
-                              <div class="col-9">
-                                  <div class="form-group form-group-default">
-                                      {{ $data->username }}
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xl-12">
+          <div class="card overflow-hidden">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-3">
+                        <div class="form-group">
+                            <label>Nama</label>
+                        </div>
+                    </div>
+                    <div class="col-9">
+                        <div class="form-group form-group-default">
+                            {{ $data->name }}
+                        </div>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col-3">
+                        <div class="form-group">
+                            <label>Jabatan</label>
+                        </div>
+                    </div>
+                    <div class="col-9">
+                        <div class="form-group form-group-default">
+                            <?php echo $data->position['name']; ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3">
+                        <div class="form-group">
+                            <label>Username</label>
+                        </div>
+                    </div>
+                    <div class="col-9">
+                        <div class="form-group form-group-default">
+                            {{ $data->username }}
+                        </div>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
         <!-- ROW-1 END -->
