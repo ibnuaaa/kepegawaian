e8e8b4917dbee0a34e0e6b1d3a6d975112aca5f9
Chaisir Ibnu Supraja <pkusumanegara@gmail.com>
2022-01-18 12:54:16 +0700

    Perbaikan simpan data User

--- resources/views/app/user/profile_edit/scripts/form.blade.php
+++ resources/views/app/user/profile_edit/scripts/form.blade.php
@@ -8,19 +8,19 @@ function savePersonal(e, field) {
 
 
 
-  const data = {
-    `${field}` : 'adasd'
-  };
-
-  showLoading()
-  axios.put('/user', data).then((response) => {
-      location.reload()
-  }).catch((error) => {
-      if (Boolean(error) && Boolean(error.response) && Boolean(error.response.data) && Boolean(error.response.data.exception) && Boolean(error.response.data.exception.message)) {
-          swal({ title: 'Opps!', text: error.response.data.exception.message, type: 'error', confirmButtonText: 'Ok' })
-          hideLoading()
-      }
-  })
+  // const data = {
+  //   `${field}` : 'adasd'
+  // };
+  //
+  // showLoading()
+  // axios.put('/user', data).then((response) => {
+  //     location.reload()
+  // }).catch((error) => {
+  //     if (Boolean(error) && Boolean(error.response) && Boolean(error.response.data) && Boolean(error.response.data.exception) && Boolean(error.response.data.exception.message)) {
+  //         swal({ title: 'Opps!', text: error.response.data.exception.message, type: 'error', confirmButtonText: 'Ok' })
+  //         hideLoading()
+  //     }
+  // })
 }
 
 function saveNewUserPendidikan() {
