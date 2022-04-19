a07ddea4dfc5d286425681c856d6d3751a15c40e
root <root@localhost.localdomain>
2022-04-19 08:49:47 +0700

    Menambah menu download iku

--- routes/cms.php
+++ routes/cms.php
@@ -82,6 +82,7 @@ $router->get('/penilaian_prestasi_kerja/id/{penilaian_prestasi_kerja_id}', 'CMS\
 $router->get('/penilaian_prestasi_kerja', 'CMS\PenilaianPrestasiKerja\PenilaianPrestasiKerjaController@Home');
 $router->get('/penilaian_prestasi_kerja/edit/{id}', 'CMS\PenilaianPrestasiKerja\PenilaianPrestasiKerjaController@Edit');
 $router->get('/penilaian_prestasi_kerja/pdf/{id}', 'CMS\PenilaianPrestasiKerja\PenilaianPrestasiKerjaController@Pdf');
+$router->get('/penilaian_prestasi_kerja/pdf_iku/{id}', 'CMS\PenilaianPrestasiKerja\PenilaianPrestasiKerjaController@PdfIku');
 $router->get('/penilaian_prestasi_kerja/{id}', 'CMS\PenilaianPrestasiKerja\PenilaianPrestasiKerjaController@Detail');
 $router->get('/penilaian_prestasi_kerja/logbook/{id}', 'CMS\PenilaianPrestasiKerja\PenilaianPrestasiKerjaController@Logbook');
 
