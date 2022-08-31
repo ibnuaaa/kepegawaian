2cc410ac74119e0c73fcf00571760e884df4ca78
Chaisir Ibnu Supraja <pkusumanegara@gmail.com>
2022-08-31 09:32:27 +0700

    dashboard

--- resources/views/app/home/scripts/form.blade.php
+++ resources/views/app/home/scripts/form.blade.php
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 
     axios.get('/penilaian_prestasi_kerja/tahun/{{ date('Y') }}/bulan/{{ date('m') }}/set/first/user_id/my').then((response) => {
-      $('#dashboard_iframe').attr('src', '{{ getConfig('protocol') }}://{{ getConfig('basepath') }}/penilaian_prestasi_kerja/pdf/' + response.data.data.records.id);
+      if (response.data.data && response.data.data.records && response.data.data.records.id) $('#dashboard_iframe').attr('src', '{{ getConfig('protocol') }}://{{ getConfig('basepath') }}/penilaian_prestasi_kerja/pdf/' + response.data.data.records.id);
     }).catch((error) => {
         if (Boolean(error) && Boolean(error.response) && Boolean(error.response.data) && Boolean(error.response.data.exception) && Boolean(error.response.data.exception.message)) {
             swal({ title: 'Opps!', text: error.response.data.exception.message, type: 'error', confirmButtonText: 'Ok' })
